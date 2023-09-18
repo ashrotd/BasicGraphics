9288e9c61e15d91e5e57b62c8c003124c21b1fa1
ashrotd <dhakalrabin04@gmail.com>
2023-09-18 00:12:30 -0400

    Handle Errors in GLSL

---
+++ error_handling_GLSL.cpp
@@ -0,0 +1,42 @@
+// Since GLSL runs in c++ runtime its difficult
+// to find the error in the compilation
+// or linking or simple OpenGL errors
+
+// These 3 functions help in finding the errors
+
+//Program 2.3 Modules to Catch GLSL Errors
+
+void printShaderLog(GLuint shader) {
+    int len = 0;
+    int chWrittn = 0;
+    char *log;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+    if (len > 0) {
+        log = (char *)malloc(len);
+        glGetShaderInfoLog(shader, len, &chWrittn, log);
+        cout << "Shader Info Log: " << log << endl;
+        free(log);
+    } 
+}
+void printProgramLog(int prog) {
+    int len = 0;
+    int chWrittn = 0;
+    char *log;
+    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+    if (len > 0) {
+        log = (char *)malloc(len);
+        glGetProgramInfoLog(prog, len, &chWrittn, log);
+        cout << "Program Info Log: " << log << endl;
+        free(log);
+    } 
+}
+bool checkOpenGLError() {
+    bool foundError = false;
+    int glErr = glGetError();
+    while (glErr != GL_NO_ERROR) {
+        cout << "glError: " << glErr << endl;
+        foundError = true;
+        glErr = glGetError();
+    }
+return foundError;
+}
